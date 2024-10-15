9a896806f2aa30c960f23cf054f00fa87ac97703
TsukiSo <133663388+TsukiSo@users.noreply.github.com>
2024-10-15 17:17:36 +0200

    MOCK: Frontend should provide data samples

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import data from '/Users/solalodh/FAMILINK/frontend/sample_data.json'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(data)
   return (
     <>
       <div>
