edb05fc9756da6dffb88ada2b5d40fc5508931a2
TsukiSo <133663388+TsukiSo@users.noreply.github.com>
2025-01-12 22:39:45 +0100

    test

--- frontend/src/Components/Discussion.jsx
+++ frontend/src/Components/Discussion.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 
 export default function Discussion({ onSelectChat }) {
     const [discussions, setDiscussions] = useState([]);
+    onSelectChat("Aurelien")
 
-    useEffect(() => {
+    /*useEffect(() => {
         fetch('http://localhost:5984/familink/_design/newDesignDoc/_view/chat?start_key=%5B%22Momo%22%5D&end_key=%5B%22Momo%22%2C%20%7B%7D%20%5D&reduce=true&group=true&group_level=2')  
             .then(response => response.json())
             .then(data => {
@@ -16,26 +17,24 @@ export default function Discussion({ onSelectChat }) {
             })
             .catch(error => console.error('Erreur de chargement des discussions:', error));
     }, []);
-
+    
     const handleDisc = (event) => {
         const senderName = event.currentTarget.querySelector('.chat-sender');
         if (senderName) {
             onSelectChat(senderName.textContent);  
         }
     };
-
+    */
     return (
         <div className="sidebar">
             <div className="sidebar-title-container">
                 <h2 className="sidebar-title">Discussions</h2>
             </div>
-            {discussions.map((x, index) => (
-                <div key={index} onClick={handleDisc} className="chat-box">
-                    <p className="chat-sender">{x}</p>
+                <div key="test" className="chat-box">
+                    <p className="chat-sender">Aurelien</p>
                     <p className="chat-message">Message</p> {/* Message à ajouter plus tard */}
                     <p className="chat-time">2024</p> {/* Timestamp à ajouter plus tard */}
                 </div>
-            ))}
         </div>
     );
 }
